7b373bfb893f35353ffdbdb4400f5abf5c0480b5
Martin Mendoza <martinmendozadev@gmail.com>
2025-04-23 00:18:43 -0600

    Add: Initialize CDN images configuration

---
+++ config/initializers/cdn_images.rb
@@ -0,0 +1,6 @@
+# config/initializers/cdn_images.rb
+
+CDN_IMAGES = {
+  logo: "https://res.cloudinary.com/dtezbna78/image/upload/v1745384932/StateForce/x0jvdecxhs3w2mmpbens.svg",
+  hero: "https://res.cloudinary.com/dtezbna78/image/upload/v1745384932/StateForce/x0jvdecxhs3w2mmpbens.svg"
+}.freeze
