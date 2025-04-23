28ca6100f985f2ade2329e33da9b30492c6272ea
Martin Mendoza <martinmendozadev@gmail.com>
2025-04-22 22:26:09 -0600

    Fix: Update landing page test to use root URL

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
   test "should get landing" do
-    get pages_landing_url
+    get root_url
     assert_response :success
   end
 end
