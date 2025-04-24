708a57343da88b46d2c78054d17f290407ea02e1
Martin Mendoza <martinmendozadev@gmail.com>
2025-04-23 19:45:57 -0600

    Update: Refactor landing page layout and CDN images configuration

--- config/initializers/cdn_images.rb
+++ config/initializers/cdn_images.rb
@@ -1,6 +1,7 @@
 # config/initializers/cdn_images.rb
 
 CDN_IMAGES = {
+  arrow_right_icon: "https://res.cloudinary.com/dtezbna78/image/upload/v1745457865/StateForce/wvqztm3hwqcv6yqkvtng.svg",
   logo: "https://res.cloudinary.com/dtezbna78/image/upload/v1745384932/StateForce/x0jvdecxhs3w2mmpbens.svg",
-  hero: "https://res.cloudinary.com/dtezbna78/image/upload/v1745384932/StateForce/x0jvdecxhs3w2mmpbens.svg"
+  hero: "https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2830&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply"
 }.freeze
