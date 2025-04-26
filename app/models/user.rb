6759bcb5655064af3cca2269382ec15ea693f7f0
Martin Mendoza <martinmendozadev@gmail.com>
2025-04-26 01:54:41 -0600

    Update: enhance user model with password validation and improve seed data generation for random users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,22 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-  :recoverable, :rememberable, :validatable,
-  :omniauthable, omniauth_providers: [ :google_oauth2 ]
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: [ :google_oauth2 ]
 
   # enum role: { guest: 0, standard: 1, admin: 2 }
+
+  # Pasword validation
+  validates :password, presence: true, if: -> { provider.blank? && uid.blank? }
+
+  # Devise pasword validation
+  def password_required?
+    provider.blank? && uid.blank? && super
+  end
+
+  def email_required?
+    super
+  end
 
   def self.from_omniauth(auth)
     where(email: auth.info.email).first_or_create do |user|
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,43 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# frozen_string_literal: true
+
+require 'faker'
+
+# Crear usuarios aleatorios
+10.times do
+  login_google = rand(0..1)
+
+  if login_google == 1
+    google_auth = Faker::Omniauth.google
+
+    User.find_or_create_by!(email: Faker::Internet.unique.email) do |user|
+      user.avatar_url = google_auth[:info][:image]
+      user.first_name = google_auth[:info][:first_name]
+      user.last_name = google_auth[:info][:last_name]
+      user.uid = google_auth[:uid]
+      user.provider = google_auth[:provider]
+      user.role = rand(0..2)
+    end
+  else
+    User.find_or_create_by!(email: Faker::Internet.unique.email) do |user|
+      user.password = '123qweASD'
+      user.password_confirmation = '123qweASD'
+      user.first_name = Faker::Name.first_name
+      user.last_name = Faker::Name.last_name
+      user.phone = Faker::PhoneNumber.phone_number
+      user.role = rand(0..2)
+    end
+  end
+end
+
+# Default user config to Development
+User.find_or_create_by!(email: 'test@stateforce.mx') do |user|
+  user.confirmed_at = Faker::Time.between(from: 2.days.ago, to: Time.zone.today)
+  user.first_name = 'Test'
+  user.last_name = 'User'
+  user.password = '123qweASD'
+  user.password_confirmation = '123qweASD'
+  user.phone = '+525555555555'
+  user.role = 2
+end
+
+Rails.logger.debug 'Seed data created successfully!'
