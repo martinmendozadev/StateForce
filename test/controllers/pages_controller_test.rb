cf1f111f1085dc39b6b44026bfb491c5c83d85e9
Martin Mendoza <martinmendozadev@gmail.com>
2025-04-26 01:26:34 -0600

    Update: add tests for landing page elements including login, signup buttons, title, subtitle, and learn more button

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -5,4 +5,29 @@ class PagesControllerTest < ActionDispatch::IntegrationTest
     get root_url
     assert_response :success
   end
+
+  test "should have login button" do
+    get root_url
+    assert_select "a[href='#{new_user_session_path}']", text: /#{I18n.t("landing.nav.login")}/
+  end
+
+  test "should have signup button" do
+    get root_url
+    assert_select "a[href='#{new_user_registration_path}']", text: /#{I18n.t("landing.hero.cta.primary")}/
+  end
+
+  test "should have title" do
+    get root_url
+    assert_select "h1", text: /#{I18n.t("landing.hero.title")}/
+  end
+
+  test "should have subtitle" do
+    get root_url
+    assert_select "p", text: /#{I18n.t("landing.hero.subtitle")}/
+  end
+
+  test "should have learn more button" do
+    get root_url
+    assert_select "a[href='#']", text: /#{I18n.t("landing.nav.learn_more")}/
+  end
 end
