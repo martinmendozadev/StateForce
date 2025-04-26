179178b412b525a07f000a6c3f81a243ac5c1b12
Martin Mendoza <martinmendozadev@gmail.com>
2025-04-25 23:11:28 -0600

    Add: google icon url

--- config/initializers/cdn_images.rb
+++ config/initializers/cdn_images.rb
@@ -2,6 +2,7 @@
 
 CDN_IMAGES = {
   arrow_right_icon: "https://res.cloudinary.com/dtezbna78/image/upload/v1745457865/StateForce/wvqztm3hwqcv6yqkvtng.svg",
-  logo: "https://res.cloudinary.com/dtezbna78/image/upload/v1745384932/StateForce/x0jvdecxhs3w2mmpbens.svg",
-  hero: "https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2830&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply"
+  google_icon: "https://res.cloudinary.com/dtezbna78/image/upload/v1745643825/StateForce/cr2u3vvlrewj2oftsmsj.svg",
+  hero: "https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2830&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply",
+  logo: "https://res.cloudinary.com/dtezbna78/image/upload/v1745384932/StateForce/x0jvdecxhs3w2mmpbens.svg"
 }.freeze
