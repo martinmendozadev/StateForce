cf1347b2f608572a8babd97094e4fca2ceda47aa
Martin Mendoza <martinmendozadev@gmail.com>
2025-04-25 21:25:50 -0600

    Add: user authentication with Google OAuth2 and Devise integration
    - Created Users::OmniauthCallbacksController to handle Google OAuth2 callbacks.
    - Added Users::OmniauthCallbacksHelper for potential helper methods.
    - Implemented User model with Devise modules and omniauthable configuration.
    - Created views for Devise confirmations, mailer templates, password management, and user registrations.
    - Configured Devise initializer with default settings and added OmniAuth configuration for Google.
    - Updated routes to include Devise for user authentication and OmniAuth callbacks.
    - Added migration for users table with necessary fields for authentication and user details.
    - Generated schema file reflecting the new users table structure.
    - Created test files for Users::OmniauthCallbacksController and User model.
    - Added user fixtures for testing purposes.

---
+++ app/models/user.rb
@@ -0,0 +1,21 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+  :recoverable, :rememberable, :validatable,
+  :omniauthable, omniauth_providers: [ :google_oauth2 ]
+
+  # enum role: { guest: 0, standard: 1, admin: 2 }
+
+  def self.from_omniauth(auth)
+    where(email: auth.info.email).first_or_create do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.first_name = auth.info.first_name
+      user.last_name = auth.info.last_name
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+      user.avatar_url = auth.info.image
+    end
+  end
+end
