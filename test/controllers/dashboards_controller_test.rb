e7464f24760bb4fdeae0d915b111a4c82b3dae8a
Martin Mendoza <martinmendozadev@gmail.com>
2025-04-27 11:48:11 -0600

    Update: enhance dashboard controller tests for user session management

--- test/controllers/dashboards_controller_test.rb
+++ test/controllers/dashboards_controller_test.rb
@@ -1,6 +1,24 @@
 require "test_helper"
 
 class DashboardsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
+  def setup
+    get user_session_path
+    @user = users(:user_one)
+    sign_in @user
+    post user_session_path
+  end
+
+  test "should not be able to access dashboard without login" do
+    sign_out @user
+    get dashboard_url
+    assert_redirected_to new_user_session_path
+    follow_redirect!
+    assert_response :success
+    assert_select "h2", "Sign in"
+  end
+
   test "should get index" do
     get dashboard_url
     assert_response :success
