0da5dbc514145ac6ec61a315c11faf66f43cc099
Martin Mendoza <martinmendozadev@gmail.com>
2025-04-27 12:28:47 -0600

    Add: integration tests for user sign-up functionality

---
+++ test/controllers/user_signup_test.rb
@@ -0,0 +1,55 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class UserSignupTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = {
+      last_name: Faker::Name.last_name,
+      first_name: Faker::Name.first_name,
+      email: Faker::Internet.email,
+      password: "password123",
+      password_confirmation: "password123"
+    }
+  end
+
+  test "user can sign up with valid information" do
+    get new_user_registration_path
+    assert_response :success
+
+    assert_difference "User.count", 1 do
+      post user_registration_path, params: { user: @user }
+    end
+
+    follow_redirect!
+    assert_response :success
+    assert_select "h2", I18n.t("instructions.title")
+
+    created_user = User.find_by(email: @user[:email])
+
+    assert_not_nil created_user, "User was not stored in the database"
+
+    assert_equal @user[:email], created_user.email
+
+    assert created_user.valid_password?(@user[:password]), "Password was not stored correctly"
+  end
+
+  test "user cannot sign up with invalid information" do
+    get new_user_registration_path
+    assert_response :success
+
+    assert_no_difference "User.count" do
+      post user_registration_path, params: {
+        user: {
+          name: Faker::Name.name,
+          email: Faker::Internet.email,
+          password: "password123",
+          password_confirmation: "123password"
+        }
+      }
+    end
+
+    assert_response :unprocessable_entity
+    assert_select "div", "Password confirmation doesn't match Password"
+  end
+end
