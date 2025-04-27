70924edbe2716671e55ddedf85b879ca7f3a66f9
Martin Mendoza <martinmendozadev@gmail.com>
2025-04-27 12:11:43 -0600

    Add: sign-in system tests for user authentication

---
+++ test/system/sign_in_test.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require "test_helper"
+require "faker"
+
+class SignInTest < ActionDispatch::SystemTestCase
+  def setup
+    @user = users(:user_one)
+  end
+
+  test "user can sign in" do
+    visit new_user_session_path
+
+    fill_in I18n.t("devise.registrations.email"), with: @user.email
+    fill_in I18n.t("devise.registrations.password"), with: "password"
+    click_on I18n.t("devise.sessions.sign_in")
+
+    assert_current_path dashboard_path
+  end
+
+  test "user cannot sign in with invalid credentials" do
+    visit new_user_session_path
+
+    fill_in I18n.t("devise.registrations.email"), with: @user.email
+    fill_in I18n.t("devise.registrations.password"), with: "wrong_password"
+    click_on I18n.t("devise.sessions.sign_in")
+
+    assert_text I18n.t("devise.failure.not_found_in_database", authentication_keys: "Email")
+  end
+end
