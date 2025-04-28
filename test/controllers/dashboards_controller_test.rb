ac10702e3ccaa098402dc8c9cc63afaec6c9c39a
Martin Mendoza <martinmendozadev@gmail.com>
2025-04-28 00:33:24 -0600

    Refactor: update dashboard controller tests for clarity and organization

--- test/controllers/dashboards_controller_test.rb
+++ test/controllers/dashboards_controller_test.rb
@@ -7,7 +7,12 @@ class DashboardsControllerTest < ActionDispatch::IntegrationTest
     get user_session_path
     @user = users(:user_one)
     sign_in @user
-    post user_session_path
+    # post user_session_path
+  end
+
+  test "should get index" do
+    get dashboard_url
+    assert_response :success
   end
 
   test "should not be able to access dashboard without login" do
@@ -19,8 +24,18 @@ class DashboardsControllerTest < ActionDispatch::IntegrationTest
     assert_select "h2", "Sign in"
   end
 
-  test "should get index" do
+  test "should not be accessible to non-verified email users" do
+    sign_out @user
+
+    @unconfirmed_user = users(:user_three)
+    sign_in @unconfirmed_user
+
     get dashboard_url
+
+    assert_redirected_to users_instructions_path
+    follow_redirect!
+
     assert_response :success
+    assert_select "h2", I18n.t("instructions.title")
   end
 end
