6f1c950fab1fd89ce8bedb26c5189f9d540fe532
Martin Mendoza <martinmendozadev@gmail.com>
2025-04-28 13:05:26 -0600

    Update: dashboard tests

--- test/controllers/dashboards_controller_test.rb
+++ test/controllers/dashboards_controller_test.rb
@@ -11,17 +11,17 @@ class DashboardsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get index" do
-    get dashboard_url
+    get dashboard_path
     assert_response :success
   end
 
   test "should not be able to access dashboard without login" do
     sign_out @user
-    get dashboard_url
+
+    get dashboard_path
     assert_redirected_to new_user_session_path
     follow_redirect!
     assert_response :success
-    assert_select "h2", "Sign in"
   end
 
   test "should not be accessible to non-verified email users" do
@@ -30,12 +30,10 @@ class DashboardsControllerTest < ActionDispatch::IntegrationTest
     @unconfirmed_user = users(:user_three)
     sign_in @unconfirmed_user
 
-    get dashboard_url
+    get dashboard_path
 
     assert_redirected_to users_instructions_path
     follow_redirect!
-
     assert_response :success
-    assert_select "h2", I18n.t("instructions.title")
   end
 end
