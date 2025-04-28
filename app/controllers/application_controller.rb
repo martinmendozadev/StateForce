8396b0f78415c65e489c3cacb921df347fe4be01
Martin Mendoza <martinmendozadev@gmail.com>
2025-04-28 13:46:05 -0600

    Refactor: enhance user authentication flow by improving confirmation checks and redirect logic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,16 +4,23 @@ class ApplicationController < ActionController::Base
 
   protected
 
-  def authenticate_user!(*args)
-    super(*args)
+  def authenticate_confirmed_user!
+    authenticate_user!
+
     return unless user_signed_in?
 
-    if user_signed_in? && !current_user.confirmed? && !devise_controller? && request.path != users_instructions_path
-      redirect_to users_instructions_path, alert: t("devise.failure.unconfirmed")
+    unless current_user.confirmed?
+      sign_out(current_user)
+      redirect_to users_instructions_path, alert: t("devise.confirmations.send_instructions")
     end
   end
 
   def after_sign_in_path_for(resource)
-    resource.confirmed? ? dashboard_path : users_instructions_path
+    if resource.confirmed?
+      dashboard_path
+    else
+      sign_out(resource)
+      users_instructions_path
+    end
   end
 end
