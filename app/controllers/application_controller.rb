ba4aac0925e5bb09b391ff9f0c7e9a58da3a1805
Martin Mendoza <martinmendozadev@gmail.com>
2025-04-28 01:12:00 -0600

    Refactor: streamline email confirmation check in authenticate_user! method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,23 +2,18 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
-  before_action :ensure_email_confirmed, if: :user_signed_in?, unless: -> { request.path == users_instructions_path }
-
   protected
 
-  def after_sign_in_path_for(resource)
-    if resource.confirmed?
-      dashboard_path
-    else
-      users_instructions_path
+  def authenticate_user!(*args)
+    super(*args)
+    return unless user_signed_in?
+
+    if user_signed_in? && !current_user.confirmed? && !devise_controller? && request.path != users_instructions_path
+      redirect_to users_instructions_path, alert: t("devise.failure.unconfirmed")
     end
   end
 
-  private
-
-  def ensure_email_confirmed
-    unless current_user.confirmed?
-      redirect_to users_instructions_path, alert: t("devise.failure.unconfirmed")
-    end
+  def after_sign_in_path_for(resource)
+    resource.confirmed? ? dashboard_path : users_instructions_path
   end
 end
