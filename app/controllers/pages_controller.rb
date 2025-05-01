8961473526e947747df806a59f6c563b6fefbbfa
Martin Mendoza <martinmendozadev@gmail.com>
2025-05-01 14:17:04 -0600

    Add: implement learn_more action and view, update routes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
   def landing
   end
+
+  def learn_more
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "pages#landing"
-  get "/dashboard", to: "dashboards#index"
 
   devise_for :users, controllers: {
     omniauth_callbacks: "users/omniauth_callbacks"
   }
 
+  get "/dashboard", to: "dashboards#index"
+  get "/learn_more", to: "pages#learn_more"
   get "users/instructions", to: "users/instructions#instructions"
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
