3364a6fbcd5bf474f1ef973ff00c9776b7d293ae
Martin Mendoza <martinmendozadev@gmail.com>
2025-05-01 15:19:21 -0600

    Add: enhance learn more page tests for login button, title, and button link

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -6,14 +6,29 @@ class PagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+ test "should get learn more" do
+    get learn_more_path
+    assert_response :success
+  end
+
   test "should have login button" do
     get root_url
+    assert_select "a[href='#{new_user_session_path}']", text: /#{I18n.t("landing.nav.login")}/
+  end
+
+  test "should have login button learn more" do
+    get learn_more_path
     assert_select "a[href='#{new_user_session_path}']", text: /#{I18n.t("landing.nav.login")}/
   end
 
   test "should have signup button" do
     get root_url
     assert_select "a[href='#{new_user_registration_path}']", text: /#{I18n.t("landing.hero.cta.primary")}/
+  end
+
+  test "should have title learn more" do
+    get learn_more_path
+    assert_select "h1", text: /#{I18n.t("learn_more.title")}/
   end
 
   test "should have title" do
@@ -28,6 +43,6 @@ class PagesControllerTest < ActionDispatch::IntegrationTest
 
   test "should have learn more button" do
     get root_url
-    assert_select "a[href='#']", text: /#{I18n.t("landing.nav.learn_more")}/
+    assert_select "a[href='#{learn_more_path}']", text: /#{I18n.t("landing.nav.learn_more")}/
   end
 end
