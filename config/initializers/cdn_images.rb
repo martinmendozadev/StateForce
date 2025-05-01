f89778e024d947c1a6e058091d521d57865840d8
Martin Mendoza <martinmendozadev@gmail.com>
2025-05-01 14:17:49 -0600

    Add: include additional CDN images for logos and icons

--- config/initializers/cdn_images.rb
+++ config/initializers/cdn_images.rb
@@ -2,7 +2,15 @@
 
 CDN_IMAGES = {
   arrow_right_icon: "https://res.cloudinary.com/dtezbna78/image/upload/v1745457865/StateForce/wvqztm3hwqcv6yqkvtng.svg",
+  firefighter_icon: "https://res.cloudinary.com/dtezbna78/image/upload/v1746127214/StateForce/bomberos-ciudad-de-mexico-logo-png_seeklogo-21095_iocutc.png",
+  ccec_logo: "https://res.cloudinary.com/dtezbna78/image/upload/v1746130914/StateForce/qsrc6o2luty3s0cgqowq.jpg",
+  ceccom_logo: "https://res.cloudinary.com/dtezbna78/image/upload/v1746127215/StateForce/ychStdPD_400x400_hx4ity.jpg",
+  cemer_logo: "https://res.cloudinary.com/dtezbna78/image/upload/v1746126582/StateForce/67eaf8617cd603ce4f915f95_cemer_logo_xbyboz.png",
+  civil_protection_logo: "https://res.cloudinary.com/dtezbna78/image/upload/v1746126582/StateForce/67047f6caf1bae7e72953992_PROT_CIVIL_2_LOGO__drm8q0.png",
   google_icon: "https://res.cloudinary.com/dtezbna78/image/upload/v1745643825/StateForce/cr2u3vvlrewj2oftsmsj.svg",
   hero: "https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2830&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply",
-  logo: "https://res.cloudinary.com/dtezbna78/image/upload/v1745384932/StateForce/x0jvdecxhs3w2mmpbens.svg"
+  imss_logo: "https://res.cloudinary.com/dtezbna78/image/upload/v1746127214/StateForce/1200px-Logotipo_del_IMSS.svg_glrd0v.png",
+  issste_logo: "https://res.cloudinary.com/dtezbna78/image/upload/v1746127215/StateForce/ISSSTE_logo_dv9ihc.png",
+  logo: "https://res.cloudinary.com/dtezbna78/image/upload/v1745384932/StateForce/x0jvdecxhs3w2mmpbens.svg",
+  state_morelos: "https://res.cloudinary.com/dtezbna78/image/upload/v1746127214/StateForce/6716ac7d9152a5e837ae8564_LOGOS_SALUD_V6-p-500_rakywb.png"
 }.freeze
