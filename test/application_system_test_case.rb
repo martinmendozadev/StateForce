afa7014710141e0008b1c91d1fa0882822e3caeb
Martin Mendoza <martinmendozadev@gmail.com>
2025-05-25 02:29:04 -0600

    Update: Enhance Chrome options for system tests with additional arguments and preferences

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -6,11 +6,16 @@ class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-gpu")
     options.add_argument("--disable-site-isolation-trials")
+    options.add_argument("--disable-features=PasswordCheck,AutofillServerCommunication,AutofillKeyMetrics")
+    options.add_argument("--disable-popup-blocking")
+    options.add_argument("--disable-extensions")
     options.add_argument("--headless=new")
+    options.add_argument("--incognito")
 
     prefs = {
       "credentials_enable_service" => false,
-      "profile.password_manager_enabled" => false
+      "profile.password_manager_enabled" => false,
+      "safebrowsing.enabled" => false
     }
     options.add_preference(:prefs, prefs)
   end
