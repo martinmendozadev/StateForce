2a944104053e5110c8e9ce7219dc93cd964a9a7b
Martin Mendoza <martinmendozadev@gmail.com>
2025-05-25 13:43:29 -0600

    Add: initial dashboard test to verify user email and logout button presence

---
+++ test/system/dashboard_test.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require "test_helper"
+require "faker"
+
+class DashboardTest < ActionDispatch::SystemTestCase
+  def setup
+    @user = users(:user_one)
+    sign_in @user
+  end
+
+  test "dashboard shows user email and logout button" do
+    visit dashboard_path
+
+    assert_text @user.email
+
+    assert_selector "form[action='#{destroy_user_session_path}'] button", text: I18n.t("devise.sessions.logout")
+  end
+end
