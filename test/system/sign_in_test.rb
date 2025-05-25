c89fa5a6c1326b320da5e9230776e2c33eed8f58
Martin Mendoza <martinmendozadev@gmail.com>
2025-05-25 12:32:08 -0600

    Refactor: Reorganize sign-in tests for improved readability and structure

--- test/system/sign_in_test.rb
+++ test/system/sign_in_test.rb
@@ -6,11 +6,36 @@ require "faker"
 class SignInTest < ActionDispatch::SystemTestCase
   def setup
     @user = users(:user_one)
+
+    visit new_user_session_path
+  end
+
+  test "renders the sign-in page" do
+    assert_selector "h2", text: I18n.t("devise.sessions.sign_in")
+  end
+
+  test "has link to return to home page" do
+    assert_selector "a[href='#{root_path}']"
+  end
+
+  test "renders sign-in form elements" do
+    assert_selector "form[action='#{user_session_path}'][method='post']"
+    assert_selector "input[name='user[email]']"
+    assert_selector "input[name='user[password]']"
+    assert_selector "input[type='submit'][value='#{I18n.t("devise.sessions.sign_in") }']"
+  end
+
+  test "sign-in page shows Google sign-in button" do
+    assert_selector "a[href='#{user_google_oauth2_omniauth_authorize_path}']"
+  end
+
+  test "sign-in page has link to sign-up page" do
+    assert_text I18n.t("devise.sessions.create_account").strip
+    assert_text I18n.t("devise.registrations.sign_up").strip
+    assert_selector "a[href='#{new_user_registration_path}']"
   end
 
   test "user can sign in" do
-    visit new_user_session_path
-
     fill_in I18n.t("devise.registrations.email"), with: @user.email
     fill_in I18n.t("devise.registrations.password"), with: "password"
     click_on I18n.t("devise.sessions.sign_in")
@@ -19,8 +44,6 @@ class SignInTest < ActionDispatch::SystemTestCase
   end
 
   test "user cannot sign in with invalid credentials" do
-    visit new_user_session_path
-
     fill_in I18n.t("devise.registrations.email"), with: @user.email
     fill_in I18n.t("devise.registrations.password"), with: "wrong_password"
     click_on I18n.t("devise.sessions.sign_in")
