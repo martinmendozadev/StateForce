10d00de188820b21b660669311da0e0d3b2ba866
Martin Mendoza <martinmendozadev@gmail.com>
2025-06-29 20:18:59 -0600

    Refactor: Simplify ApplicationMailerTest by removing unnecessary instance creation

--- test/mailers/application_mailer_test.rb
+++ test/mailers/application_mailer_test.rb
@@ -2,12 +2,10 @@ require "test_helper"
 
 class ApplicationMailerTest < ActionMailer::TestCase
   test "default from email is set correctly" do
-    mail = ApplicationMailer.new
-    assert_equal "from@example.com", mail.default[:from], "Default from email should be set to from@example.com"
+    assert_equal "from@example.com", ApplicationMailer.default[:from], "Default from email should be set to from@example.com"
   end
 
   test "layout is set to mailer" do
-    mail = ApplicationMailer.new
-    assert_equal "mailer", mail.class.default[:layout], "Layout should be set to mailer"
+    assert_equal "mailer", ApplicationMailer.default[:layout], "Layout should be set to mailer"
   end
 end
