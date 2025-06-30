b6112a95a9a7e612781528aa389f1adbd3eba2e8
Martin Mendoza <martinmendozadev@gmail.com>
2025-06-29 23:02:19 -0600

    Fix: Update ApplicationJob to include argument in perform_later calls in tests

--- test/jobs/application_job_test.rb
+++ test/jobs/application_job_test.rb
@@ -3,14 +3,14 @@ require "test_helper"
 class ApplicationJobTest < ActiveJob::TestCase
   test "job is enqueued successfully" do
     assert_enqueued_with(job: ApplicationJob) do
-      ApplicationJob.perform_later
+      ApplicationJob.perform_later({ foo: "bar" })
     end
   end
 
   test "job performs successfully" do
     assert_nothing_raised do
       perform_enqueued_jobs do
-        ApplicationJob.perform_later
+        ApplicationJob.perform_later({ foo: "bar" })
       end
     end
   end
