a5e8cff8d7f86a13f11e9e5d0666057980861eb5
Martin Mendoza <martinmendozadev@gmail.com>
2025-06-29 23:25:09 -0600

    Refactor: Simplify ApplicationJob and update tests for enqueuing behavior

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -1,15 +1,9 @@
 # frozen_string_literal: true
 
 class ApplicationJob < ActiveJob::Base
-   queue_as :default
   # Automatically retry jobs that encountered a deadlock
   # retry_on ActiveRecord::Deadlocked
 
   # Most jobs are safe to ignore if the underlying records are no longer available
   # discard_on ActiveJob::DeserializationError
-
-  def perform(*args)
-    # Basic implementation for testing purposes
-    Rails.logger.info("ApplicationJob performed with arguments: #{args.inspect}")
-  end
 end
--- test/jobs/application_job_test.rb
+++ test/jobs/application_job_test.rb
@@ -1,37 +1,13 @@
 require "test_helper"
 
 class ApplicationJobTest < ActiveJob::TestCase
+  test "job class is defined" do
+    assert ApplicationJob.new.is_a?(ApplicationJob)
+  end
+
   test "job is enqueued successfully" do
     assert_enqueued_with(job: ApplicationJob) do
-      ApplicationJob.perform_later({ foo: "bar" })
+      ApplicationJob.perform_later
     end
-  end
-
-  test "job is enqueued with default queue" do
-    assert_enqueued_with(job: ApplicationJob, queue: "default") do
-      ApplicationJob.perform_later({ foo: "bar" })
-    end
-  end
-
-  test "job performs successfully" do
-    assert_nothing_raised do
-      perform_enqueued_jobs do
-        ApplicationJob.perform_later({ foo: "bar" })
-      end
-    end
-  end
-
-  test "job logs arguments when performed" do
-    log_output = StringIO.new
-    Rails.logger = Logger.new(log_output)
-
-    perform_enqueued_jobs do
-      ApplicationJob.perform_later({ foo: "bar" })
-    end
-
-    log_output.rewind
-    assert_match(/ApplicationJob performed with arguments: \[\{foo: \"bar\"\}\]/, log_output.read)
-  ensure
-    Rails.logger = ActiveSupport::Logger.new(STDOUT) # Restaurar el logger original
   end
 end
