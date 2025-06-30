07115962676a09b4564134dc0351e5c0ecf17e3c
Martin Mendoza <martinmendozadev@gmail.com>
2025-06-29 23:11:27 -0600

    Fix: Add test for job enqueued with default queue in ApplicationJobTest

--- test/jobs/application_job_test.rb
+++ test/jobs/application_job_test.rb
@@ -7,9 +7,23 @@ class ApplicationJobTest < ActiveJob::TestCase
     end
   end
 
+  test "job is enqueued with default queue" do
+    assert_enqueued_with(job: ApplicationJob, queue: "default") do
+      ApplicationJob.perform_later({ foo: "bar" })
+    end
+  end
+
   test "job performs successfully" do
     assert_nothing_raised do
       perform_enqueued_jobs do
+        ApplicationJob.perform_later({ foo: "bar" })
+      end
+    end
+  end
+
+  test "job logs arguments when performed" do
+    perform_enqueued_jobs do
+      assert_logs(/ApplicationJob performed with arguments: \[{:foo=>"bar"}\]/) do
         ApplicationJob.perform_later({ foo: "bar" })
       end
     end
