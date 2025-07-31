f211997298d01d4152bcdcc501927474e727a159
Martin Mendoza <martinmendozadev@gmail.com>
2025-07-31 01:10:10 -0600

    Refactor: remove role enum and update user model to use 'name' for authentication; adjust validations accordingly

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,17 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :confirmable,
          :omniauthable, omniauth_providers: [ :google_oauth2 ]
 
-  enum :role, guest: 0, standard: 1, admin: 2
 
+  ## Validations
+  validates :email, presence: true, length: { maximum: 150 }, uniqueness: true
+  validates :name,  length: { maximum: 75 }, allow_blank: true
   # Ensure password is required only when setting or updating it, and skip for provider-based users
   validates :password, presence: true, if: -> { new_record? || (password.present? && provider.blank?) }, unless: -> { provider.present? && uid.present? }
+
+
+  ## Enums
+  enum :provider, google_oauth2: 0
+
 
   def password_required?
     provider.blank? && uid.blank? && super
@@ -26,8 +33,7 @@ class User < ApplicationRecord
     where(email: auth.info.email).first_or_create do |user|
       user.provider = auth.provider
       user.uid = auth.uid
-      user.first_name = auth.info.first_name
-      user.last_name = auth.info.last_name
+      user.name = auth.info.name
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
       user.avatar_url = auth.info.image
