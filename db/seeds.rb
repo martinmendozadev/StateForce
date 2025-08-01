ce76f4d6bda2da39a94af5423019c4e1e67f1cda
Martin Mendoza <martinmendozadev@gmail.com>
2025-08-01 17:48:06 -0600

    Refactor: update seed data to assign avatar_url for Google-authenticated users and improve attachment creation logic

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,14 +3,13 @@
 require 'faker'
 
 # Create fake user
-9.times do
+users = 9.times do
   login_google = rand(0..1)
 
   if login_google == 1
     google_auth = Faker::Omniauth.google
 
     User.find_or_create_by!(email: google_auth[:info][:email]) do |user|
-      user.avatar_url = google_auth[:info][:image]
       user.name = google_auth[:info][:name]
       user.uid = google_auth[:uid]
       user.provider = google_auth[:provider]
@@ -24,6 +23,25 @@ require 'faker'
       user.confirmed_at = Faker::Time.between(from: 15.days.ago, to: Time.zone.today)
     end
   end
+end
+
+# Create fake attachments
+9.times do
+  file_type = Attachment.file_types.keys.sample
+  visibility = Attachment.visibilities.keys.sample
+
+  Attachment.create(
+    content_type: Faker::File.mime_type,
+    description: Faker::Lorem.sentence(word_count: 10),
+    file_name: Faker::File.unique.file_name(dir: 'uploads', ext: file_type.to_s),
+    file_size: Faker::Number.between(from: 1_000, to: 10_000),
+    file_type:,
+    file_url: Faker::Internet.unique.url(host: 'stateforce.mx', path: "/uploads/#{file_type}/#{Faker::File.unique.file_name(dir: 'uploads', ext: file_type.to_s)}"),
+    visibility:,
+    uploader_user_id: 1,
+    created_at: Faker::Time.between(from: 15.days.ago, to: Time.zone.today),
+    updated_at: Faker::Time.between(from: 15.days.ago, to: Time.zone.today)
+  )
 end
 
 # Default user config to Development
