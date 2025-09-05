bfb04b3cbf3c6bf30b5d6d8392577445f0a1430f
Martin Mendoza <martinmendozadev@gmail.com>
2025-09-04 23:01:41 -0600

    Refactor: correct primary key definition and enhance uniqueness validation message in OperationalUnitCompetency model

--- app/models/operational_unit_competency.rb
+++ app/models/operational_unit_competency.rb
@@ -1,14 +1,20 @@
 # frozen_string_literal: true
 
 class OperationalUnitCompetency < ApplicationRecord
-  self.primary_key = [ :operational_unit_id, :competency_id ]
+  self.primary_keys = %i[operational_unit_id competency_id]
 
-  # Associations
+  ## Associations
   belongs_to :operational_unit
   belongs_to :competency
 
-  # Validations
+  ## Validations
   validates :operational_unit, presence: true
   validates :competency, presence: true
-  validates :operational_unit_id, uniqueness: { scope: :competency_id }
+  validates :operational_unit_id, uniqueness: {
+    scope: :competency_id,
+    message: "association with this competency already exists"
+  }
+
+  ## Scopes
+  scope :active, -> { where(deleted_at: nil) }
 end
