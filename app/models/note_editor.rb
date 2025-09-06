866946e10d58598d30b4cf5e751e07bc74c62882
Martin Mendoza <martinmendozadev@gmail.com>
2025-09-05 22:07:37 -0600

    Refactor: comment out validation for future last_edited_at in NoteEditor model

--- app/models/note_editor.rb
+++ app/models/note_editor.rb
@@ -13,7 +13,7 @@ class NoteEditor < ApplicationRecord
   validates :user, presence: true
   validates :user_id, uniqueness: { scope: :note_id, message: "already assigned as editor for this note" }
   validates :last_edited_at, timeliness: { type: :datetime }, allow_nil: true
-  validate :last_edited_at_cannot_be_in_future, if: -> { last_edited_at.present? }
+  # validate :last_edited_at_cannot_be_in_future, if: -> { last_edited_at.present? }
 
   private
 
