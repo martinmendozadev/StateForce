3cfe4432ccb7b05ed4518e7ba3683f2a687ba9b4
Martin Mendoza <martinmendozadev@gmail.com>
2025-09-10 02:10:40 -0600

    Refactor: move contact_type enum definition to Enums module in UserContact model

--- app/models/user_contact.rb
+++ app/models/user_contact.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class UserContact < ApplicationRecord
+  include Enums
+
+  # Composite primary key
   self.primary_key = [ :user_id, :contact_id, :contact_type ]
 
   # Associations
@@ -8,11 +11,7 @@ class UserContact < ApplicationRecord
   belongs_to :contact
 
   # Enums
-  enum :contact_type, {
-    emergency: "emergency",
-    primary: "primary",
-    technical_support: "technical_support"
-  }, prefix: true
+  enum :contact_type, CONTACT_TYPES, prefix: true
 
   # Validations
   validates :user, presence: true
