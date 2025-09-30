5ce3b1007103c14080b700c653b599613ff3dab4
Martin Mendoza <martinmendozadev@gmail.com>
2025-09-30 01:05:12 -0600

    Refactor: improve ImageHelper tests by enhancing setup and teardown processes

--- test/helpers/image_helper_test.rb
+++ test/helpers/image_helper_test.rb
@@ -8,17 +8,25 @@ class ImageHelperTest < ActionView::TestCase
   setup do
     @icons_dir = Rails.root.join("app/assets/images/icons")
     FileUtils.mkdir_p(@icons_dir)
+    @created = []
   end
 
   teardown do
-    # Clean up any test svg we created
-    test_svg = @icons_dir.join("test_icon.svg")
-    File.delete(test_svg) if File.exist?(test_svg)
+    @created.each do |file|
+      File.delete(file) if File.exist?(file)
+    end
+  end
+
+  def write_icon(name, content)
+    path = @icons_dir.join("#{name}.svg")
+    File.write(path, content)
+    @created << path
+    path
   end
 
   test "cdn_image returns image tag for symbol key" do
     html = cdn_image(:logo_white, alt: "Logo")
-    assert_includes html, "img"
+    assert_includes html, "<img"
     assert_includes html, CDN_IMAGES[:logo_white]
     assert_includes html, "alt=\"Logo\""
   end
@@ -35,6 +43,7 @@ class ImageHelperTest < ActionView::TestCase
     url = "https://example.com/x.png"
     html = cdn_image(url, alt: "X")
     assert_includes html, url
+    assert_includes html, "alt=\"X\""
   end
 
   test "cdn_image normalizes atl typo to alt" do
@@ -43,41 +52,47 @@ class ImageHelperTest < ActionView::TestCase
     assert_includes html, "alt=\"Typo\""
   end
 
-  test "svg_icon inlines svg and injects fill and class" do
-    svg_source = <<~SVG
-      <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M0 0h24v24H0z" fill="none"/></svg>
-    SVG
-    File.write(@icons_dir.join("test_icon.svg"), svg_source)
-
-    html = svg_icon(:test_icon, class: "h-5 w-5 text-primary")
-    assert_includes html, "<svg"
-    assert_includes html, "fill=\"currentColor\""
-    assert_includes html, "class=\"h-5 w-5 text-primary\""
+  test "svg_icon returns nil when missing locally and no CDN fallback" do
+    assert_nil svg_icon(:totally_missing_icon, title: "Nada")
   end
 
-  test "svg_icon adds title and role img when title present" do
-    svg_source = <<~SVG
-      <svg viewBox="0 0 10 10" xmlns="http://www.w3.org/2000/svg"></svg>
-    SVG
-    File.write(@icons_dir.join("test_icon.svg"), svg_source)
-
-    html = svg_icon(:test_icon, title: "Prueba", class: "x")
-    assert_includes html, "<title>Prueba</title>"
-    assert_includes html, "role=\"img\""
-    refute_includes html, "aria-hidden"
-  end
-
-  test "svg_icon falls back to cdn image when file missing and key exists" do
-    # Use :google (expects google_icon in CDN) and ensure no local file exists
-    File.delete(@icons_dir.join("google.svg")) if File.exist?(@icons_dir.join("google.svg"))
-
+  test "svg_icon uses CDN fallback when local file absent but *_icon key exists" do
     html = svg_icon(:google, title: "Google")
-    assert_includes html, CDN_IMAGES[:google_icon], "Should use CDN fallback"
+    assert_includes html, CDN_IMAGES[:google_icon]
     assert_includes html, "alt=\"Google\""
   end
 
-  test "svg_icon returns nil when file missing and no cdn fallback" do
-    html = svg_icon(:__not_existing_anywhere__, title: "Nada")
-    assert_nil html
+  test "svg_icon strips existing class on root svg before injecting new ones" do
+    write_icon(:strip_test, '<svg class="old" viewBox="0 0 1 1" xmlns="http://www.w3.org/2000/svg"></svg>')
+    html = svg_icon(:strip_test, class: "new-class")
+    refute_includes html, 'class="old"'
+    assert_includes html, 'class="new-class"'
+  end
+
+  test "svg_icon injects fill=currentColor when svg missing fill attribute" do
+    write_icon(:fill_test, '<svg viewBox="0 0 1 1" xmlns="http://www.w3.org/2000/svg"></svg>')
+    html = svg_icon(:fill_test)
+    assert_includes html, 'fill="currentColor"'
+  end
+
+  test "svg_icon does not override existing fill attribute" do
+    write_icon(:fill_keep, '<svg fill="red" viewBox="0 0 1 1" xmlns="http://www.w3.org/2000/svg"></svg>')
+    html = svg_icon(:fill_keep)
+    assert_includes html, 'fill="red"'
+    refute_match(/fill="currentColor".*fill="red"/, html)
+  end
+
+  test "svg_icon with title adds role img and removes aria-hidden" do
+    write_icon(:title_test, '<svg viewBox="0 0 1 1" xmlns="http://www.w3.org/2000/svg"></svg>')
+    html = svg_icon(:title_test, title: "Titulo")
+    assert_includes html, '<title>Titulo</title>'
+    assert_includes html, 'role="img"'
+    refute_includes html, 'aria-hidden'
+  end
+
+  test "svg_icon without title keeps aria-hidden" do
+    write_icon(:hidden_test, '<svg viewBox="0 0 1 1" xmlns="http://www.w3.org/2000/svg"></svg>')
+    html = svg_icon(:hidden_test)
+    assert_includes html, 'aria-hidden="true"'
   end
 end
