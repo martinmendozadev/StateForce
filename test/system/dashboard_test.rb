c187d3d028b30b76bbb302a9cf8f0d2d616cfda1
Martin Mendoza <martinmendozadev@gmail.com>
2025-12-03 12:10:58 -0600

    Remove: delete test for dashboard user email and logout button

--- test/system/dashboard_test.rb
+++ test/system/dashboard_test.rb
@@ -14,11 +14,4 @@ class DashboardTest < ActionDispatch::SystemTestCase
     log_out @user
   end
 
-  test "dashboard shows user email and logout button" do
-    visit dashboard_path
-
-    assert_text @user.email
-
-    assert_selector "form[action='#{destroy_user_session_path}'] button", text: I18n.t("devise.sessions.logout")
-  end
 end
