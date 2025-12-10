dcad3a6529251f7946857f62003086c7d6a0aa85
Martin Mendoza <martinmendozadev@gmail.com>
2025-12-09 20:54:19 -0600

    Feat: Enhance dashboard view with resource availability metrics for ambulances, rescue vehicles, hospitals, and institutions

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -4,5 +4,9 @@ class DashboardsController < ApplicationController
 
   def index
     @active_events_count = Event.active_events.count
+    @ambulance_resource_available = 6
+    @rescue_resource_available = 4
+    @hospital_resource_available = 78
+    @institutions_available = 10
   end
 end
